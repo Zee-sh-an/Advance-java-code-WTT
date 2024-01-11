c9b17db28a0b1be35a1d562e7bba68020793c3d7
Zeeshan <zeeshan3167@gmail.com>
2024-01-11 19:48:01 +0530

    deleting .idea folder of main folder(Advance-java-code) and (for endless loop of hql query to get whole data from db)

--- Hibernate/src/main/java/com/hibernate/App.java
+++ Hibernate/src/main/java/com/hibernate/App.java
@@ -7,6 +7,7 @@ import jakarta.persistence.criteria.CriteriaQuery;
 import jakarta.persistence.criteria.Root;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.query.Query;
 
@@ -18,7 +19,6 @@ public class App {
         try {
 
             SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
-            System.out.println(sessionFactory);
 
             Customer customer = new Customer();
 //        customer.setId(45);
@@ -31,37 +31,40 @@ public class App {
             order.setPrice(56.7);
 
             customer.setOrder(order);
+            order.setCustomer(customer);
 
             Session session = sessionFactory.openSession();
 //            Transaction transaction = session.beginTransaction();
 
-            CriteriaBuilder builder = session.getCriteriaBuilder();
-            CriteriaQuery<Customer> criteriaQuery = builder.createQuery(Customer.class);
-            Root root = criteriaQuery.from(Customer.class);
-
-
-            criteriaQuery.select(root);
-            criteriaQuery.where(builder.like(root.get("order").get("name"), "Mouse"));
-            criteriaQuery.where(builder.equal(root.get("id"), 2));
-            Query<Customer> customerQuery = session.createQuery(criteriaQuery);
-            List customerList = customerQuery.getResultList();
-            for (Object c1 : customerList) {
-                System.out.println(c1);
-            }
+//            CriteriaBuilder builder = session.getCriteriaBuilder();
+//            CriteriaQuery<Customer> criteriaQuery = builder.createQuery(Customer.class);
+//            Root root = criteriaQuery.from(Customer.class);
+//
+//
+//            criteriaQuery.select(root);
+//            criteriaQuery.where(builder.like(root.get("order").get("name"), "Mouse"));
+//            criteriaQuery.where(builder.equal(root.get("id"), 2));
+//            Query<Customer> customerQuery = session.createQuery(criteriaQuery);
+//            List customerList = customerQuery.getResultList();
+//            for (Object c1 : customerList) {
+//                System.out.println(c1);
+//            }
 
             System.out.println("___________________________________________________________________________");
 
 
 //            session.save(customer);
 //            transaction.commit();
+//        comment the toString customerOrder field otherwise it will create a endless loop for calling all customers in db
 
 //        String query1="select c from Customer c where c.email =:email";
-//        Query q1=session.createQuery(query1);
+            String query1 ="from Customer";
+        Query q1=session.createQuery(query1);
 //        q1.setParameter("email","zeeshan3167@gmail.com");
-//        List<Customer> list1=q1.list();
-//        for (Customer c: list1) {
-//            System.out.println(c);
-//        }
+        List list1=q1.list();
+        for (Object c: list1) {
+            System.out.println(c);
+        }
 
             System.out.println("___________________________________________________________________________");
 //
